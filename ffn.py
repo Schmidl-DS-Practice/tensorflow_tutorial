80e696ddf98479844bc350eb35ca6c376782a6a8
scottschmidl <sjschmidl@gmail.com>
2022-05-04 22:18:01 -0500

    finished

--- ffn.py
+++ ffn.py
@@ -18,36 +18,45 @@ def main():
 
     # model
     model = models.Sequential([layers.Flatten(input_shape=(28,28)),
-                                    layers.Dense(128, activation="relu"),
-                                    layers.Dense(10)])
+                               layers.Dense(128, activation="relu"),
+                               layers.Dense(10)])
     print(model.summary())
 
     ## another way to build the Sequential model:
-    # model = keras.models.Sequential()
-    # model.add(keras.layers.Flatten(input_shape=(28,28))
-    # model.add(keras.layers.Dense(128, activation='relu'))
-    # model.add(keras.layers.Dense(10))
+    # model = models.Sequential()
+    # model.add(layers.Flatten(input_shape=(28,28))
+    # model.add(layers.Dense(128, activation='relu'))
+    # model.add(layers.Dense(10))
 
     ## loss and optimizer
     loss = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
     optim = keras.optimizers.Adam(lr=0.001)
     metrics = ["accuracy"]
 
-    model.compile(loss=loss, optimizer=optim, metrics=metrics)
+    model.compile(loss=loss, optimizer=optim,
+                  metrics=metrics)
 
     ## training
-    batch_size = 64
+    bs = 64 # batch_size
     epochs = 5
 
-    model.fit(xtrain, ytrain, batch_size=batch_size, epochs=epochs, shuffle=True, verbose=2)
+    model.fit(xtrain,
+              ytrain,
+              batch_size=bs,
+              epochs=epochs,
+              shuffle=True,
+              verbose=2)
 
     ## evaulate
-    model.evaluate(xtest, ytest, batch_size=batch_size, verbose=2)
+    model.evaluate(xtest,
+                   ytest,
+                   batch_size=bs,
+                   verbose=2)
 
     ## predictions
-
     ## 1. option: build new model with Softmax layer
-    probability_model = models.Sequential([model, layers.Softmax()])
+    probability_model = models.Sequential([model,
+                                           layers.Softmax()])
 
     predictions = probability_model(xtest)
     pred0 = predictions[0]
@@ -58,26 +67,26 @@ def main():
     print(label0)
 
     ## 2. option: original model + nn.softmax, call model(x)
-    predictions = model(xtest)
-    predictions = tf.nn.softmax(predictions)
-    pred0 = predictions[0]
-    print(pred0)
-    label0 = np.argmax(pred0)
-    print(label0)
+    # predictions = model(xtest)
+    # predictions = tf.nn.softmax(predictions)
+    # pred0 = predictions[0]
+    # print(pred0)
+    # label0 = np.argmax(pred0)
+    # print(label0)
 
     ## 3. option: original model + nn.softmax, call model.predict(x)
-    predictions = model.predict(xtest, batch_size=batch_size)
-    predictions = tf.nn.softmax(predictions)
-    pred0 = predictions[0]
-    print(pred0)
-    label0 = np.argmax(pred0)
-    print(label0)
+    # predictions = model.predict(xtest, batch_size=bs)
+    # predictions = tf.nn.softmax(predictions)
+    # pred0 = predictions[0]
+    # print(pred0)
+    # label0 = np.argmax(pred0)
+    # print(label0)
 
     ## call argmax for multiple labels
-    pred05s = predictions[0:5]
-    print(pred05s.shape)
-    label05s = np.argmax(pred05s, axis=1)
-    print(label05s)
+    # pred05s = predictions[0:5]
+    # print(pred05s.shape)
+    # label05s = np.argmax(pred05s, axis=1)
+    # print(label05s)
 
 if __name__ == "__main__":
     main()
